68cb4c8ae298959ba4c862e896b834533661c1f0
Danail <DanailMinchev@users.noreply.github.com>
2022-11-02 17:52:47 +0000

    fix: action fixes

--- dist/index.js
+++ dist/index.js
@@ -16773,6 +16773,68 @@ const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
 const axios = __nccwpck_require__(8757)
 
+const createCapybaraRun = async (
+  capybaraUrl,
+  clientId,
+  location,
+  owner,
+  repoName,
+  workflowId,
+  ref,
+  durationInMinutes,
+  maximumDelayInSeconds
+) => {
+  try {
+    const capybaraInstance = axios.create({
+      baseURL: capybaraUrl,
+      timeout: 60000,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+
+    const response = await capybaraInstance.post("/v1/github/workflows/runs", {
+      clientId: clientId,
+      repository: {
+        owner: owner,
+        name: repoName,
+        workflowId: workflowId,
+        ref: ref,
+      },
+      schedule: {
+        location: location,
+        approximateWorkflowRunDurationInMinutes: durationInMinutes,
+        maximumDelayInSeconds: maximumDelayInSeconds,
+      },
+    })
+    return response.scheduledTime
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const cancelGitHubRun = async (owner, repoName) => {
+  try {
+    const githubInstance = axios.create({
+      baseURL: "https://api.github.com/",
+      timeout: 5000,
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${core.getInput("github-token")}`,
+      },
+    })
+
+    const response = await githubInstance.post(
+      `/repos/${owner}/${repoName}/actions/runs/${github.context.runId}/cancel`
+    )
+
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 try {
   const isCapybaraDispatch =
     github.context.payload.inputs &&
@@ -16782,7 +16844,7 @@ try {
       : false
 
   if (!isCapybaraDispatch) {
-    // The workflow is not triggered by Capybara server - call the server and fail the build. Otherwise, ignore.
+    // The workflow is not triggered by Capybara backend - call the backend and cancel the build. Otherwise, ignore.
     const capybaraUrl = core.getInput("capybaraUrl")
     const clientId = core.getInput("clientId")
     const workflowId = core.getInput("workflowId")
@@ -16793,60 +16855,20 @@ try {
     const maximumDelayInSeconds = core.getInput("maximumDelayInSeconds")
     const location = core.getInput("location")
 
-    const capybaraInstance = axios.create({
-      baseURL: capybaraUrl,
-      timeout: 60000,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    })
-    // workflow dispatches
-    let bestTimeToStart
-
-    capybaraInstance
-      .post("/v1/github/workflows/runs", {
-        clientId: clientId,
-        repository: {
-          owner: owner,
-          name: repoName,
-          workflowId: workflowId,
-          ref: ref,
-        },
-        schedule: {
-          location: location,
-          approximateWorkflowRunDurationInMinutes: durationInMinutes,
-          maximumDelayInSeconds: maximumDelayInSeconds,
-        },
-      })
-      .then(function (response) {
-        bestTimeToStart = response.scheduledTime
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-
-    const githubInstance = axios.create({
-      baseURL: "https://api.github.com/",
-      timeout: 5000,
-      headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: `Bearer ${core.getInput("github-token")}`,
-      },
-    })
-
-    githubInstance
-      .post(
-        `/repos/${owner}/${repoName}/actions/runs/${github.context.runId}/cancel`
-      )
-      .then(function (response) {
-        console.log(response)
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-
+    const bestTimeToStart = createCapybaraRun(
+      capybaraUrl,
+      clientId,
+      location,
+      owner,
+      repoName,
+      workflowId,
+      ref,
+      durationInMinutes,
+      maximumDelayInSeconds
+    )
     core.setOutput("bestTimeToStart", bestTimeToStart)
+
+    cancelGitHubRun(owner, repoName)
   }
 } catch (error) {
   core.setFailed(error.message)
--- index.js
+++ index.js
@@ -2,6 +2,68 @@ const core = require("@actions/core")
 const github = require("@actions/github")
 const axios = require("axios")
 
+const createCapybaraRun = async (
+  capybaraUrl,
+  clientId,
+  location,
+  owner,
+  repoName,
+  workflowId,
+  ref,
+  durationInMinutes,
+  maximumDelayInSeconds
+) => {
+  try {
+    const capybaraInstance = axios.create({
+      baseURL: capybaraUrl,
+      timeout: 60000,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+
+    const response = await capybaraInstance.post("/v1/github/workflows/runs", {
+      clientId: clientId,
+      repository: {
+        owner: owner,
+        name: repoName,
+        workflowId: workflowId,
+        ref: ref,
+      },
+      schedule: {
+        location: location,
+        approximateWorkflowRunDurationInMinutes: durationInMinutes,
+        maximumDelayInSeconds: maximumDelayInSeconds,
+      },
+    })
+    return response.scheduledTime
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const cancelGitHubRun = async (owner, repoName) => {
+  try {
+    const githubInstance = axios.create({
+      baseURL: "https://api.github.com/",
+      timeout: 5000,
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${core.getInput("github-token")}`,
+      },
+    })
+
+    const response = await githubInstance.post(
+      `/repos/${owner}/${repoName}/actions/runs/${github.context.runId}/cancel`
+    )
+
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 try {
   const isCapybaraDispatch =
     github.context.payload.inputs &&
@@ -11,7 +73,7 @@ try {
       : false
 
   if (!isCapybaraDispatch) {
-    // The workflow is not triggered by Capybara server - call the server and fail the build. Otherwise, ignore.
+    // The workflow is not triggered by Capybara backend - call the backend and cancel the build. Otherwise, ignore.
     const capybaraUrl = core.getInput("capybaraUrl")
     const clientId = core.getInput("clientId")
     const workflowId = core.getInput("workflowId")
@@ -22,60 +84,20 @@ try {
     const maximumDelayInSeconds = core.getInput("maximumDelayInSeconds")
     const location = core.getInput("location")
 
-    const capybaraInstance = axios.create({
-      baseURL: capybaraUrl,
-      timeout: 60000,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    })
-    // workflow dispatches
-    let bestTimeToStart
-
-    capybaraInstance
-      .post("/v1/github/workflows/runs", {
-        clientId: clientId,
-        repository: {
-          owner: owner,
-          name: repoName,
-          workflowId: workflowId,
-          ref: ref,
-        },
-        schedule: {
-          location: location,
-          approximateWorkflowRunDurationInMinutes: durationInMinutes,
-          maximumDelayInSeconds: maximumDelayInSeconds,
-        },
-      })
-      .then(function (response) {
-        bestTimeToStart = response.scheduledTime
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-
-    const githubInstance = axios.create({
-      baseURL: "https://api.github.com/",
-      timeout: 5000,
-      headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: `Bearer ${core.getInput("github-token")}`,
-      },
-    })
-
-    githubInstance
-      .post(
-        `/repos/${owner}/${repoName}/actions/runs/${github.context.runId}/cancel`
-      )
-      .then(function (response) {
-        console.log(response)
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-
+    const bestTimeToStart = createCapybaraRun(
+      capybaraUrl,
+      clientId,
+      location,
+      owner,
+      repoName,
+      workflowId,
+      ref,
+      durationInMinutes,
+      maximumDelayInSeconds
+    )
     core.setOutput("bestTimeToStart", bestTimeToStart)
+
+    cancelGitHubRun(owner, repoName)
   }
 } catch (error) {
   core.setFailed(error.message)
